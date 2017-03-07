ee20cb1a662b06aa15f5175c55d92585d66bc3c3
DanYellow <jeanlouis.danielo@sfr.fr>
2017-03-07 22:21:08 +0100

    update arch

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,15 @@
-var path = require('path');
+var path = require('path')
 
 var FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'public')
+    filename: 'public/index.js',
+    publicPath: '/',
+    pathinfo: true,
+    path: path.resolve(__dirname, '/')
   },
   module: {
     rules: [{
@@ -17,18 +20,28 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
     },{
-            test: /\.scss$/,
-            use: [{
-                loader: 'style-loader'
-            }, {
-                loader: 'css-loader'
-            }, {
-                loader: 'sass-loader'
-            }]
-        }
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'sass-loader'
+      }]
+    }, {
+      test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+      use: [{
+        loader: 'file-loader',
+        query: {
+          name: 'public/images/[name].[hash:8].[ext]'
+        },
+      }],
+        
+    }
     ]
   },
   plugins: [
     new FlowBabelWebpackPlugin(),
+    // new CleanWebpackPlugin(['public']),
   ],
 }
